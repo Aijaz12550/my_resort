d31b0c2d9e8be18b3ee66cb9f967b49fa7df7c6e
Muhammad Aijaz <muhammadaijaz76@gmail.com>
2019-12-13 17:50:13 +0500

    Update firebase.js

--- src/Config/firebase.js
+++ src/Config/firebase.js
@@ -7,13 +7,7 @@ import { promised } from 'q';
 
 const config ={
 
-    apiKey: "AIzaSyDujSCDDO_r4RGLhyYk5Eklo5zkWfADwx4",
-    authDomain: "dream-world-d2cc2.firebaseapp.com",
-    databaseURL: "https://dream-world-d2cc2.firebaseio.com",
-    projectId: "dream-world-d2cc2",
-    storageBucket: "dream-world-d2cc2.appspot.com",
-    messagingSenderId: "1030010325982",
-    appId: "1:1030010325982:web:b35a557c4cdf4337"
+   
 }
 const app = firebase.initializeApp(config)
 // function login(email,password){
